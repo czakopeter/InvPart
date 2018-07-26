2d59a885e19b0f14888478f6e877329c2671305b
PeterPC <PeterPC@TESZTGP007-PC>
2018-07-26 15:49:41 +0200

    Uptade menu bar.

--- src/invpart/Constants.java
+++ src/invpart/Constants.java
@@ -4,4 +4,8 @@ public class Constants {
   public static String WINDOWS_TITLE = "Invoice partitioner";
   public static int WINDOWS_MIN_WIDTH = 700;
   public static int WINDOWS_MIN_HEIGHT = 600;
+  public static String MENU_SETTINGS = "Adatbevitel/Beállítások";
+  public static String MENU_LICENSE_PLATE = "Rendszam";
+  public static String MENU_REPORT = "Jelentés";
+  public static String MENU_EXIT = "Kilépés";
 }
--- src/invpart/ViewController.java
+++ src/invpart/ViewController.java
@@ -2,16 +2,39 @@ package invpart;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.StackPane;
 
 public class ViewController implements Initializable {
+  @FXML
+  public StackPane menuPane;
+  
+  private void setMenuBar() {
+    TreeItem<String> root = new TreeItem<>();
+    
+    TreeItem<String> settings = new TreeItem<>(Constants.MENU_SETTINGS);
+    TreeItem<String> reports = new TreeItem<>(Constants.MENU_REPORT);
+    TreeItem<String> exit = new TreeItem<>(Constants.MENU_EXIT);
+    
+    settings.getChildren().addAll(
+            new TreeItem<>(Constants.MENU_LICENSE_PLATE)
+    );
+    reports.getChildren().addAll();
+    
+    
+    root.getChildren().addAll(settings,reports,exit);
+    
+    TreeView<String> treeView = new TreeView<>(root);
+    treeView.setShowRoot(false);
+    menuPane.getChildren().addAll(treeView);
+  }
   
   @Override
   public void initialize(URL url, ResourceBundle rb) {
-    // TODO
+    setMenuBar();
   }  
   
 }
